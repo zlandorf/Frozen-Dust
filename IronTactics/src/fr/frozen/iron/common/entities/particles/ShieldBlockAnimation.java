94a7041c1ec14e6421acd3366489226882fd080c
Paul Milian <paul.milian@hotmail.fr>
2011-09-18 22:41:00 +0200

    shieldBlock animation tweaked

--- IronTactics/src/fr/frozen/iron/common/entities/particles/ShieldBlockAnimation.java
+++ IronTactics/src/fr/frozen/iron/common/entities/particles/ShieldBlockAnimation.java
@@ -6,6 +6,18 @@ import fr.frozen.game.ISpriteManager;
 public class ShieldBlockAnimation extends AnimatedParticle {
 	public ShieldBlockAnimation(GameState gs, float x, float y) {
 		super(gs, x, y, ISpriteManager.getInstance().getAnimationSequence("shieldblockanimation"));
-		//TODO : add graphic animation for shield block on spritesheet
+	}
+	
+	@Override
+	public void update(float deltaTime) {
+		if (animation == null) return;
+		
+		animation.update(deltaTime);
+		if (animation.isAnimationEnded()) {
+			animation.getCurrentSprite().setAlpha(Math.max(0, animation.getCurrentSprite().getAlpha() - deltaTime * 1.5f));
+			if (animation.getCurrentSprite().getAlpha() <= 0) {
+				_gameState.removeGameObject(this);
+			}
+		}
 	}
 }
