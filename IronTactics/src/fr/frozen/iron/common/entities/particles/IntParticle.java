c5ea51f4b90c57510c78c8537dbf77122eb2f4fb
Paul Milian <paul.milian@hotmail.fr>
2011-10-07 21:12:46 +0200

    two intParticles no longer overlap

--- IronTactics/src/fr/frozen/iron/common/entities/particles/IntParticle.java
+++ IronTactics/src/fr/frozen/iron/common/entities/particles/IntParticle.java
@@ -21,6 +21,9 @@ public class IntParticle extends GameObject {
 	
 	protected Font damageFont;
 	
+	protected int width;
+	protected int height;
+	
 	public IntParticle(IronWorld world, float x, float y, int value, int negColor, int posColor) {
 		super(null, x, y);
 		this.world = world;
@@ -32,9 +35,60 @@ public class IntParticle extends GameObject {
 			str = " "+str;
 		}
 		damageFont = FontManager.getFont("DamageFont");
-		setPos((int)(getX() - damageFont.getWidth(str) / 2),(int)( getY() - damageFont.getHeight(str) / 2));
+		width = damageFont.getWidth(str);
+		height = damageFont.getHeight(str);
+		
+		setPosWithNoCollision((int)(getX() -  width / 2), (int)getY());
+		
 		this.negColor = new Color(negColor);
 		this.posColor = new Color(posColor);
+	}
+	
+	private void setPosWithNoCollision(int x, int y) {
+		setPos(x, y);
+		if (world.getGameObjectCollection("gfx") == null) return;
+		
+		for (GameObject go : world.getGameObjectCollection("gfx")) {
+			if (this.equals(go)) continue;
+			if (go instanceof IntParticle) {
+				IntParticle otherParticle = (IntParticle) go;
+				if (collides(otherParticle)) {
+					int offY = (int)(otherParticle.height - Math.abs(getY() - otherParticle.getY()));
+					otherParticle.setPos((int)otherParticle.getX(),
+										(int)(otherParticle.getY() - offY /2));
+					
+					setPos((int)getX(),(int)(getY() + offY /2));
+					break;
+				}
+			}
+		}
+		
+	}
+	
+	private boolean collides(IntParticle ip) {
+		int x1, y1, w1, h1;
+		int x2, y2, w2, h2;
+		
+		x1 = (int)getX();
+		y1 = (int)getY();
+		
+		x2 = (int)ip.getX();
+		y2 = (int)ip.getY();
+		
+		w1 = width;
+		h1 = height;
+		
+		w2 = ip.width;
+		h2 = ip.height;
+		
+		if ((x2 >= x1 + w1)
+		    || (x2 + w2 <= x1)
+			|| (y2 >= y1 + h1)
+			|| (y2 + h2 <= y1)) {
+			return false; 
+		}
+		
+		return true;
 	}
 	
 	@Override
